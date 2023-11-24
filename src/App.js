1c8e6d0c186b88db9415ede568452cbd83bd3536
aron98 <aron98@gmail.com>
2023-11-25 00:44:04 +0100

    Create folder structure (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Home from "./Pages/Home/Home";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <div>Felso Bar</div>
-        <Home />
+        <Routes>
+            <Route path="/" element={(
+                <Home />
+            )} />
+        </Routes>
         <div>Also Bar</div>
     </div>
   );
