6c61c1ebfba326a5fbf2ff29d2c8cf4184af53bd
aron98 <aron98@gmail.com>
2023-11-25 07:13:28 +0100

    Filling up news page (#11)
    * Fill up news page
    
    * Fill up news page

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,12 +1,126 @@
-import {Box} from "@mui/joy";
+import {AspectRatio, Box, Card, CardContent, CardOverflow, Divider} from "@mui/joy";
 import Typography from "@mui/joy/Typography";
+import Link from '@mui/joy/Link';
 
 const News = (props) => {
     return (
         <Box>
-            <Typography>
-                News
-            </Typography>
+            <Box p={1}>
+                <Card variant="soft">
+                    <CardOverflow>
+                        <AspectRatio ratio="2">
+                            <img
+                                src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
+                                srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+                                loading="lazy"
+                                alt=""
+                            />
+                        </AspectRatio>
+                    </CardOverflow>
+                    <CardContent>
+                        <Typography level="title-md">
+                            <Link overlay underline="none">
+                                Yosemite National Park
+                            </Link>
+                        </Typography>
+                    </CardContent>
+                    <CardOverflow variant="soft">
+                        <Divider inset="context" />
+                        <CardContent orientation="horizontal">
+                            <Typography level="body-xs">6.3k views</Typography>
+                            <Divider orientation="vertical" />
+                            <Typography level="body-xs">1 hour ago</Typography>
+                        </CardContent>
+                    </CardOverflow>
+                </Card>
+            </Box>
+            <Box p={1}>
+                <Card variant="soft">
+                    <CardOverflow>
+                        <AspectRatio ratio="2">
+                            <img
+                                src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
+                                srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+                                loading="lazy"
+                                alt=""
+                            />
+                        </AspectRatio>
+                    </CardOverflow>
+                    <CardContent>
+                        <Typography level="title-md">
+                            <Link overlay underline="none">
+                                Yosemite National Park
+                            </Link>
+                        </Typography>
+                    </CardContent>
+                    <CardOverflow variant="soft">
+                        <Divider inset="context" />
+                        <CardContent orientation="horizontal">
+                            <Typography level="body-xs">6.3k views</Typography>
+                            <Divider orientation="vertical" />
+                            <Typography level="body-xs">1 hour ago</Typography>
+                        </CardContent>
+                    </CardOverflow>
+                </Card>
+            </Box>
+            <Box p={1}>
+                <Card variant="soft">
+                    <CardOverflow>
+                        <AspectRatio ratio="2">
+                            <img
+                                src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
+                                srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+                                loading="lazy"
+                                alt=""
+                            />
+                        </AspectRatio>
+                    </CardOverflow>
+                    <CardContent>
+                        <Typography level="title-md">
+                            <Link overlay underline="none">
+                                Yosemite National Park
+                            </Link>
+                        </Typography>
+                    </CardContent>
+                    <CardOverflow variant="soft">
+                        <Divider inset="context" />
+                        <CardContent orientation="horizontal">
+                            <Typography level="body-xs">6.3k views</Typography>
+                            <Divider orientation="vertical" />
+                            <Typography level="body-xs">1 hour ago</Typography>
+                        </CardContent>
+                    </CardOverflow>
+                </Card>
+            </Box>
+            <Box p={1}>
+                <Card variant="soft">
+                    <CardOverflow>
+                        <AspectRatio ratio="2">
+                            <img
+                                src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
+                                srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+                                loading="lazy"
+                                alt=""
+                            />
+                        </AspectRatio>
+                    </CardOverflow>
+                    <CardContent>
+                        <Typography level="title-md">
+                            <Link overlay underline="none">
+                                Yosemite National Park
+                            </Link>
+                        </Typography>
+                    </CardContent>
+                    <CardOverflow variant="soft">
+                        <Divider inset="context" />
+                        <CardContent orientation="horizontal">
+                            <Typography level="body-xs">6.3k views</Typography>
+                            <Divider orientation="vertical" />
+                            <Typography level="body-xs">1 hour ago</Typography>
+                        </CardContent>
+                    </CardOverflow>
+                </Card>
+            </Box>
         </Box>
     )
 }
