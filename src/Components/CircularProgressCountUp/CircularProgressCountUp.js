b99772aae021e5fc42afaaca311cc8de84390fd4
Aron Erdelyi <aron98@gmail.com>
2023-11-25 06:27:01 +0100

    Restyle the app

---
+++ src/Components/CircularProgressCountUp/CircularProgressCountUp.js
@@ -0,0 +1,20 @@
+import {useCountUp} from "use-count-up";
+import {CircularProgress} from "@mui/joy";
+import Typography from "@mui/joy/Typography";
+
+const CircularProgressCountUp = (props) => {
+    const { value } = useCountUp({
+        isCounting: true,
+        duration: 1,
+        start: 0,
+        end: 75,
+    });
+
+    return (
+        <CircularProgress determinate value={Number(value)} sx={{ '--CircularProgress-size': '7em' }}>
+            <Typography><b>{100-value}%</b><br/>remaining</Typography>
+        </CircularProgress>
+    );
+}
+
+export default CircularProgressCountUp;
