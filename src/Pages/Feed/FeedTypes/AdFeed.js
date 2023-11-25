8ade4f7f1c26ceaf3d0ce9f0c4c340c8b9f90ba5
vmadaras <83071806+vmadaras@users.noreply.github.com>
2023-11-25 23:49:09 +0100

    Added AD feed type (#28)
    Co-authored-by: Vilmos Madaras <mdrsvilmos@gmail.com>

---
+++ src/Pages/Feed/FeedTypes/AdFeed.js
@@ -0,0 +1,46 @@
+import {AspectRatio, Box, Card, CardContent, CardOverflow, Divider} from "@mui/joy";
+import Typography from "@mui/joy/Typography";
+import Link from '@mui/joy/Link';
+
+function AdFeed(props) {
+    return (
+        <Box p={1} key={props.index}>
+            <Card variant="soft">
+                <CardOverflow>
+                    <AspectRatio ratio="2">
+                        <img
+                            src={props.feedsItem.image}
+                            alt=""
+                        />
+                    </AspectRatio>
+                </CardOverflow>
+                <CardContent>
+                    <Typography level="title-md">
+                        <Link href={props.feedsItem.link} target='_blank' overlay underline="none">
+                            {props.feedsItem.name}
+                        </Link>
+                    </Typography>
+                </CardContent>
+                <CardOverflow variant="soft">
+                    <Divider inset="context" />
+                    <CardContent orientation="horizontal">
+                        <Typography level="body-xs">{`${props.feedsItem.view_count}`}</Typography>
+                        <Divider orientation="vertical" />
+                        <Typography level="body-xs">{props.feedsItem.date}</Typography>
+                    </CardContent>
+                </CardOverflow>
+            </Card>
+        </Box>
+    )
+}
+export default AdFeed;
+
+/*
+ez a $$$$ forrás (nem a Soros)
+tips and trick
+news
+jobs
+
+Community
+achievement events
+*/
